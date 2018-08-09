891c8b9fd6e779de37e68abe6875b72d85850410
Evan Meshberg <emeshberge@gmail.com>
2018-08-09 23:38:01 +0000

    Lesson 8 completed

--- app.js
+++ app.js
@@ -43,13 +43,29 @@ console.log(clonedBanner);
 
 // Lesson 7: Traversing the DOM Part 1
 
+// const bookList = document.querySelector('#book-list');
+// console.log('the parent node is:', bookList.parentNode);
+// console.log('the parent node is:', bookList.parentElement.parentElement);
+
+// console.log(bookList.childNodes);
+
+// console.log(bookList.children);
+
+// Lesson 8: Traversing the DOM Part 2
+
 const bookList = document.querySelector('#book-list');
-console.log('the parent node is:', bookList.parentNode);
-console.log('the parent node is:', bookList.parentElement.parentElement);
 
-console.log(bookList.childNodes);
+console.log('book-list next sibiling is:', bookList.nextSibling);
+console.log('book-list next element sibiling is:', bookList.nextElementSibling);
 
-console.log(bookList.children);
+console.log('book-list previous sibiling is:', bookList.previousSibling);
+console.log('book-list previous element sibiling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+
+
+
+
 
 
 
