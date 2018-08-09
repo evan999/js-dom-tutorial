99ea5b4ae6b713e9e5cb15cc54ba0499c80cb896
Evan Meshberg <emeshberge@gmail.com>
2018-08-09 23:20:15 +0000

    Lesson 6 completed

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ Array.from(books).forEach(function(book){
 const bookList = document.querySelector('#book-list');
 //bookList.innerHTML = '<h2>Books and more books...</h2>';
 bookList.innerHTML += '<p>This is how you add HTML</p>';
+
+
+// Lesson 6: Nodes
+
+// Elements are nodes
+
+// Text, comments, and comments are also nodes
+
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner node type is:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
