73a7bd60e3b60b521a46a5ae5439c9f5f57fb6ec
Evan Meshberg <emeshberge@gmail.com>
2018-08-09 02:39:24 +0000

    Lesson 2 completed

---
+++ get_element_by_id.js
@@ -0,0 +1,10 @@
+document
+document.getElementById('page-banner');
+
+var banner = document.getElementById('page-banner');
+
+banner
+
+var bookList = document.getElementById('book-list');
+
+bookList
