e4ecb25357e8ee9c32ea1bcd0451e5bf436e1d7e
Evan Meshberg <emeshberge@gmail.com>
2018-08-10 00:12:12 +0000

    Lesson 9 completed

--- app.js
+++ app.js
@@ -63,6 +63,40 @@ console.log('book-list previous element sibiling is:', bookList.previousElementS
 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
 
+// Lesson 9: Events
+
+var h2 = document.querySelector('#book-list h2');
+h2.addEventListener('click', function(e){
+    console.log(e.target);
+    console.log(e);
+});
+
+var btns = document.querySelectorAll('#book-list.delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        
+        const li = e.target.parentElement;
+        
+        li.parentNode.removeChild(li);
+        
+        
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault(); // 
+    console.log('navigation to ', e.target.textContent, ' was prevented');
+})
+
+
+
+
+
+
+
 
 
 
