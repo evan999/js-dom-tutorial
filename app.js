b05e0c624cf2604aaad19f76b29539d4f470f185
Evan Meshberg <emeshberge@gmail.com>
2018-08-14 22:44:39 +0000

    Tutorial Completed

--- app.js
+++ app.js
@@ -1,229 +1,103 @@
-var titles = document.getElementsByClassName('title');
+document.addEventListener('DOMContentLoaded', function(){
+    const list = document.querySelector('#book-list ul');
+    // delete books
 
-// console.log(Array.isArray(titles)); // checks if item is array; returns false
-// console.log(Array.isArray(Array.from(titles))); // create array from HTML collection; returns true
-
-
-Array.from(titles).forEach(function(item){
-    console.log(item);
-});
-
-
-//Note: 'titles' initially is NOT an array. It is an html collection
-
-// Lesson 5
-
-// grab all elements with a class of name
-var books = document.querySelectorAll('#book-list li .name');
-
-Array.from(books).forEach(function(book){
-    //console.log(book.textContent); //grabs text in element
-    book.textContent += ' (book title)';
-});
-
-// const bookList = document.querySelector('#book-list');
-// bookList.innerHTML = '<h2>Books and more books...</h2>';
-// bookList.innerHTML += '<p>This is how you add HTML</p>';
-
-
-// Lesson 6: Nodes
-
-// Elements are nodes
-
-// Text, comments, and comments are also nodes
-
-const banner = document.querySelector('#page-banner');
-
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner node type is:', banner.hasChildNodes());
-
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
-
-// Lesson 7: Traversing the DOM Part 1
-
-// const bookList = document.querySelector('#book-list');
-// console.log('the parent node is:', bookList.parentNode);
-// console.log('the parent node is:', bookList.parentElement.parentElement);
-
-// console.log(bookList.childNodes);
-
-// console.log(bookList.children);
-
-// Lesson 8: Traversing the DOM Part 2
-
-const bookList = document.querySelector('#book-list');
-
-console.log('book-list next sibiling is:', bookList.nextSibling);
-console.log('book-list next element sibiling is:', bookList.nextElementSibling);
-
-console.log('book-list previous sibiling is:', bookList.previousSibling);
-console.log('book-list previous element sibiling is:', bookList.previousElementSibling);
-
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
-
-// Lesson 9: Events
-
-var h2 = document.querySelector('#book-list h2');
-h2.addEventListener('click', function(e){
-    console.log(e.target);
-    console.log(e);
-});
-
-var btns = document.querySelectorAll('#book-list.delete');
-
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e){
+    list.addEventListener('click', function(e){
+        if(e.target.className == 'delete'){
+            const li = e.target.parentElement;
+            list.removeChild(li);
+        }
+    });
+    
+    
+    // Add books
+    
+    const addForm = document.forms['add-book'];
+    
+    addForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const value = addForm.querySelector('input[type="text"]').value;
+        console.log(value);
+    
+    
+    // Create Elements
+        const li = document.createElement('li');
+        const bookName = document.createElement('span');
+        const deleteBtn = document.createElement('span');
         
-        const li = e.target.parentElement;
+        // Add content
+        deleteBtn.textContent = 'delete';
+        bookName.textContent = value;
         
-        li.parentNode.removeChild(li);
+        // Add classes
+        bookName.classList.add('name');
+        deleteBtn.classList.add('delete');
         
+        // Append to DOM
+        
+        li.appendChild(bookName);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
         
     });
-});
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-    e.preventDefault(); // 
-    console.log('navigation to ', e.target.textContent, ' was prevented');
-});
-
-// Lesson 10: Event Bubbling
-
-const list = document.querySelector('#book-list ul');
-
-// delete books
-
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
-})
-
-// Lesson 11: Interacting with Forms
-
-// document.forms[0];
-// document.forms['add-book'];
-
-// Add books
-
-const addForm = document.forms['add-book'];
-
-addForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
-
-// Lesson 12: Creating Elements
-
-// Create Elements
-    const li = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
     
-    // Add content
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = value;
     
-    // Add classes
-    bookName.classList.add('name');
-    deleteBtn.classList.add('delete');
     
-    // Append to DOM
     
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
+    // Hide books
+    const hideBox = document.querySelector('#hide');
+    hideBox.addEventListener('change', function(e){
+       if(hideBox.checked){
+           list.style.display = "none"; // hide ul
+       } 
+       else{
+           list.style.display = "initial"; // or "block"
+       }
+    });
     
-});
-
-
-// Lesson 15: Checkboxes & Change Elements
-
-// Hide books
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-   if(hideBox.checked){
-       list.style.display = "none"; // hide ul
-   } 
-   else{
-       list.style.display = "initial"; // or "block"
-   }
-});
-
-// Lesson 16: Creating a Search Filter
-
-// filter books
-
-const searchBar = document.forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', function(e){
-    const term = e.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    Array.from(books).forEach(function(book){
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(term) != -1){
-            book.style.display = 'block';
-        }
-        else{
-            book.style.display = 'none';
-        }
-    })
-})
-
-// Lesson 17: Tabbed Content
-
-// tabbed Content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', function(e){
-    if(e.target.tagName = "LI"){
-        const targetPanel = document.querySelector(e.target.dataset.target);
-        panels.forEach(function(panel){
-            if(panel == targetPanel){
-                panel.classList.add('active');
+    
+    
+    // filter books
+    
+    const searchBar = document.forms['search-books'].querySelector('input');
+    searchBar.addEventListener('keyup', function(e){
+        const term = e.target.value.toLowerCase();
+        const books = list.getElementsByTagName('li');
+        Array.from(books).forEach(function(book){
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) != -1){
+                book.style.display = 'block';
             }
             else{
-                panel.classList.remove('active');
+                book.style.display = 'none';
             }
         })
-    }
-})
+    })
+    
+    
+    
+    // tabbed Content
+    const tabs = document.querySelector('.tabs');
+    const panels = document.querySelectorAll('.panel');
+    tabs.addEventListener('click', function(e){
+        if(e.target.tagName = "LI"){
+            const targetPanel = document.querySelector(e.target.dataset.target);
+            panels.forEach(function(panel){
+                if(panel == targetPanel){
+                    panel.classList.add('active');
+                }
+                else{
+                    panel.classList.remove('active');
+                }
+            })
+        }
+    })
+});
 
 
 
 
-//Lesson 13: Styles & Classes
 
-/*
-
-var li = document.querySelector('li:last-child');
-li.style.color = 'red';
-li.style.marginTop = "60px";
-
-li.className = "test";
-li.className = "test2";
-
-li.className += "test3";
-
-li.className += " test4";
-
-*/
-
-// Lesson 14: Attributes
-
-book.getAttribute('class'); // "name"
-book.setAttribute('class', 'name-2'); // sets attribute of element
-
-book.hasAttribute('class'); // true
-book.hasAttribute('href'); // false
-
-book.removeAttribute('class'); // removes selected attribute
-
-book.setAttribute('class', 'name');
 
 
 
